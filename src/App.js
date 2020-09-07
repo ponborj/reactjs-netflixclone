f459b04cc46c9b9c2e0fbc3d4ca2029c2dbf6395
Matheus Augusto <matheus.thop@gmail.com>
2020-09-06 22:05:31 -0300

    projeto finalizado

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
+
+export default function App() {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState([null]);
+  const [blackHeader, setBlackHeader] = useState(false)
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //peando a alista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      //pegando o featured
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+
+      //console.log(chosen);
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+
+
+    }
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  })
+
+
+  return (
+    <div className="page">
+
+      <Header black={blackHeader} />
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+
+
+      <footer>
+        <p>Feito  {/*com <span role="img" aria-label="coração"></span>*/}por Matheus Augusto<br /></p>
+        <h2>Créditos</h2>
+        <p><a href="/" >B7Web</a> e <a href="/" >TMDB</a></p>
+      </footer>
+
+      {(movieList.length <= 0 || featuredData.length <= 0) &&
+        <div className="loading">
+          <img src="https://cdn.lowgif.com/full/ce629026a12a85a7-the-tech-behind-netflix-s-worldwide-expansion-is-a-big-deal-for-the.gif" alt="loading" />
+        </div>
+      }
+    </div>
+  );
+}
